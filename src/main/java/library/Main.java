0c0b19eca5959a5d03bcc768351849b2ee34105c
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-04-30 11:21:17 +0600

    shift the main file

--- src/main/java/library/Main.java
+++ src/main/java/library/Main.java
@@ -1,6 +1,8 @@
-package library.core;
+package library;
 
 import javax.swing.SwingUtilities;
+
+import library.core.LibrarySystemGUI;
 
 public class Main {
     public static void main(String[] args) {
